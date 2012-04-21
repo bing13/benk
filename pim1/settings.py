7fac4417b7180db859178b7e0288756191e13a98
Bernard H <bernard9395@gmail.com>
2012-04-20 21:24:24 -0700

    improved placement of fastadd dialog box removed sort beltline on TODAY view

--- pim1/settings.py
+++ pim1/settings.py
@@ -136,6 +136,16 @@ INSTALLED_APPS = (
 )
 
 #should be default in django 1.2
-#TEMPLATE_CONTEXT_PROCESSORS = (
 #    'django.contrib.auth.context_processors.auth',
-#)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.contrib.messages.context_processors.messages",
+
+    "django.core.context_processors.request"
+
+)
