739a2aea1b717ee623bda4a441f36fe8a91c78ff
Bernard H <bernard9395@gmail.com>
2015-02-21 15:34:43 -0800

    big catch-up on dormant files

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,19 +4,22 @@ os.environ['DJANGO_SETTINGS_MODULE'] = "pim1.settings"
 
 # following needed to find south
 sys.path.append('/home/bhadmin13/python_pkgs/South-0.7.3-py2.5.egg')
-
+#sys.path.append('/home/bhadmin13/python_pkgs/')
+#sys.path.append('/home/bhadmin13/python_pkgs/lib/python2.5/site-packages/')
+#sys.path.append('/home/bhadmin13/dx.bernardhecker.com/pim1/library')
 
 ## to handle wsgi error reporting glitch
 cwd = os.getcwd()
 myapp_directory = cwd + '/pim1'
-sys.stdout = sys.stderr
+### THE FOLLOWING LINE ERRORS IF UNCOMMENTED SINCE AN
+### UNKNOWN DREAMHOST UPDATE, BEFORE 2014-05-30
+### hope the fix works without it.
+#sys.stdout = sys.stderr
 sys.path.insert(0,myapp_directory)
 sys.path.append(os.getcwd())
 
 
 from paste.exceptions.errormiddleware import ErrorMiddleware
-
-
 
 import django.core.handlers.wsgi
 
